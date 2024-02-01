c0274c6f1f29cef7d3a427ce63fddb3a45a5b0e7
Code-XT <rahuljhabokaro123@gmail.com>
2024-02-01 23:43:26 +0530

    download at max res

--- info.py
+++ info.py
@@ -9,9 +9,22 @@ def video_Info(yt):
     print("Is Age Restricted : ",yt.age_restricted)
     print("Thumbnail Url : ",yt.thumbnail_url)
     print('Stream Details :', yt.fmt_streams)
+
+def vidDownload(link):
+    youtubeObject = YouTube(link)
+    youtubeObject = youtubeObject.streams.get_highest_resolution()
+    try:
+        print("Downloading...")
+        youtubeObject.download()
+    except:
+        print("An error has occurred")
+    print("Downloaded successfully")
     
 link = input("Enter the link : ")
 yt = YouTube(link)
 
-
-video_Info(yt)
+choice = input('Select: \n1. Video Info \n2. Video Download\n')
+if choice == '1':
+    video_Info(yt)
+elif choice == '2':
+    vidDownload(link)
